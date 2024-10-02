a584a027f63060690ca017da9904c031fb9d8035
raman kumar <241ramankumar@gmail.com>
2024-10-02 13:52:36 +0530

    feat:able to send image

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -4,7 +4,7 @@ import { userData } from "../../userData";
 import api from "../../api";
 import { Button } from "../ui/button";
 
-const socket = io("http://localhost:5000"); 
+const socket = io("http://localhost:5000");
 
 const ChatBox = ({ openChat }) => {
   const [message, setMessage] = useState("");
@@ -53,13 +53,14 @@ const ChatBox = ({ openChat }) => {
     // Listen for new messages
     socket.on(
       "receiveMessage",
-      ({ roomId, message, currentUserId, contactUserId }) => {
-        console.log(selectedImage);
-        
+      ({ roomId, message, selectedImage, currentUserId, contactUserId }) => {
+      
+
         setChat((prevChat) => [
           ...prevChat, // Spread previous messages
           {
             content: message,
+            image: selectedImage,
             sender: currentUserId,
             recipient: contactUserId,
             createdAt: new Date(),
@@ -73,8 +74,6 @@ const ChatBox = ({ openChat }) => {
     };
   }, [contactUserId]);
 
-
-  
   const scrollToBottom = () => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
@@ -83,7 +82,7 @@ const ChatBox = ({ openChat }) => {
   }, [chat]);
 
   const sendMessage = async () => {
-    if (message && contactUserId) {
+    if ((message||selectedImage)  && contactUserId) {
       const data = await userData();
       const currentUserId = data._id;
       const roomId = [currentUserId, contactUserId].sort().join("_");
@@ -94,7 +93,7 @@ const ChatBox = ({ openChat }) => {
       socket.emit("sendMessage", {
         roomId,
         message,
-        // selectedImage,
+        selectedImage,
         currentUserId,
         contactUserId,
       });
@@ -115,7 +114,6 @@ const ChatBox = ({ openChat }) => {
     }
   };
 
-
   // Function to handle image selection
   const handleImageChange = (event) => {
     const file = event.target.files[0]; // Get the selected file
@@ -123,10 +121,9 @@ const ChatBox = ({ openChat }) => {
       const reader = new FileReader();
       reader.onload = () => {
         setSelectedImage(reader.result); // Set the image data as base64 string
-        console.log(reader.result);
+    
       };
       reader.readAsDataURL(file); // Read the file as a Data URL
-      
     }
   };
 
@@ -135,152 +132,199 @@ const ChatBox = ({ openChat }) => {
     setSelectedImage(null);
   };
 
+  const handleImageSend=()=>{
+    sendMessage();
+    removeImage();
+  }
   
 
   return (
     <>
-         { selectedImage && (<div className="fixed w-auto h-auto max-h-[600px] max-w-[700px] z-10 top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2  z-100 
-            rounded-2xl bg-gray-700">
-              {selectedImage && (
-                
-        <div className=" w-full h-full px-16">
-          <div className="flex my-5 items-center justify-start gap-6 ">
-            <button
-            onClick={removeImage}
-            className="  text-gray-300  text-6xl  w-6 h-6 flex items-center justify-center"
-          >
-            &times;
-          </button>
-          <h2 className="text-white text-3xl font-bold">Send Image</h2>
-          </div>
-          <img
-            src={selectedImage}
-            alt="Selected"
-            className="w-auto h-auto object-center   object-cover"
-          />
-          <div className="flex mt-5 mb-2" >
-          <input type="text" className="bg-transparent text-white w-full outline-none" placeholder="Add a Caption...." />
-          <Button className="bg-blue-600 hover:bg-blue-700">Send </Button>
-          </div>
-        
+      {selectedImage && (
+        <div className="fixed w-screen h-screen  z-10">
+        <div
+          className="fixed w-auto h-auto max-h-[600px] max-w-[700px] z-10 top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2  z-100 
+            rounded-2xl bg-gray-700"
+        >
+          {selectedImage && (
+            <div className=" w-full h-full px-16">
+              <div className="flex my-5 items-center justify-start gap-6 ">
+                <button
+                  onClick={removeImage}
+                  className="  text-gray-300  text-6xl  w-6 h-6 flex items-center justify-center"
+                >
+                  &times;
+                </button>
+                <h2 className="text-white text-3xl font-bold">Send Image</h2>
+              </div>
+              <img
+                src={selectedImage}
+                alt="Selected"
+                className="w-auto h-auto object-center   object-cover"
+              />
+              <div className="flex mt-5 mb-2">
+                <input
+                  type="text"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="bg-transparent text-white w-full outline-none"
+                  placeholder="Add a Caption...."
+                />
+                <Button className="bg-blue-600 hover:bg-blue-700" onClick={handleImageSend}>Send </Button>
+              </div>
+            </div>
+          )}
+        </div>
         </div>
       )}
-            </div>)}
 
-    <div className="flex flex-col w-2/3 h-screen bg-gray-900 text-white ">
-      {/* Top NavBar */}
-      <div className="flex items-center justify-between  p-4 bg-gray-800">
-        <div className="flex items-center space-x-4">
-          <button className="p-2 rounded-lg bg-gray-700">
-            <i className="fas fa-arrow-left"></i>
-          </button>
-          <div className="flex flex-col">
-            <h2 className="text-lg font-bold">{ChatuserData.name}</h2>
-            <span className="text-sm text-gray-400">last seen at 6:46 PM</span>
+      <div className="flex flex-col w-2/3 h-screen bg-gray-900 text-white ">
+        {/* Top NavBar */}
+        <div className="flex items-center justify-between  p-4 bg-gray-800">
+          <div className="flex items-center space-x-4">
+            <button className="p-2 rounded-lg bg-gray-700">
+              <i className="fas fa-arrow-left"></i>
+            </button>
+            <div className="flex flex-col">
+              <h2 className="text-lg font-bold">{ChatuserData.name}</h2>
+              <span className="text-sm text-gray-400">
+                last seen at 6:46 PM
+              </span>
+            </div>
+          </div>
+
+          {/* Search bar and 3-dot menu */}
+          <div className="flex items-center space-x-4">
+            <button className="p-2 rounded-lg bg-gray-700">
+              <i className="fas fa-search"></i>
+            </button>
+            <button className="p-2 rounded-lg bg-gray-700">
+              <i className="fas fa-ellipsis-v"></i>
+            </button>
           </div>
         </div>
 
-        {/* Search bar and 3-dot menu */}
-        <div className="flex items-center space-x-4">
-          <button className="p-2 rounded-lg bg-gray-700">
-            <i className="fas fa-search"></i>
-          </button>
-          <button className="p-2 rounded-lg bg-gray-700">
-            <i className="fas fa-ellipsis-v"></i>
-          </button>
-        </div>
-      </div>    
-
-
-
-      {/* Chat Body */}
-      <div id="chatBox" className="flex-grow p-4   bg-gray-900 overflow-y-auto">
-        {chat.map((msg, idx) => {
-          const createdAtDate = new Date(msg.createdAt);
-
-          const time = !isNaN(createdAtDate.getTime())
-            ? createdAtDate.toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: true,
-              })
-            : "Invalid Date";
-
-          return (
-            <div
-              key={idx}
-              id="outer-msg-box"
-              className={`${
-                msg.recipient === contactUserId
-                  ? "flex justify-end"
-                  : "flex justify-start"
-              }`}
-            >
-              {msg.sender === contactUserId ? (
-                <div className="m-5 p-4 relative text-xl text-white h-auto   max-w-[200px] bg-[#474545] rounded-lg">
-                  {msg.content}
-                  <div className="w-full text-xs flex items-end justify-end absolute text-gray-200 right-2 bottom-0">
-                    {time}
-                  </div>
-                </div>
-              ) : (
-                <div className="m-5 p-4 relative text-xl text-white h-auto  max-w-[200px] bg-[#2d7d4a] rounded-lg">
-                  {msg.content}
-                  <div className="w-full text-xs flex items-end justify-end absolute text-gray-200 right-2 bottom-0">
-                    {time}
-                  </div>
-                </div>
-              )}
-            </div>
-          );
-        })}
-        <div ref={chatEndRef} />
-      </div>
-
-
-
-      {/* Bottom Input Section */}
-      
-      <div className="flex items-center p-4 bg-gray-800">
-        <div className="flex items-center space-x-4">
-          <button className="p-2 text-gray-400 hover:text-white">
-            <i className="far fa-smile"></i>
-          </button>
-          <button className="p-2 text-gray-400 hover:text-white">
-            <div className="flex items-center justify-center">
-              <label className="cursor-pointer">
-                <i className="fas fa-camera text-3xl text-gray-500 hover:text-blue-500"></i>
-                <input
-                  type="file"
-                  className="hidden"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                />
-             
-              </label>
-     
-            </div>
-          </button>
-         
-        </div>
-        
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-          placeholder="Type a message"
-          className="flex-grow bg-gray-700 text-white rounded-lg px-4 py-2 mx-4 focus:outline-none"
-        />
-        <button
-          onClick={sendMessage}
-          className="bg-green-500 p-3 rounded-full hover:bg-green-600"
+        {/* Chat Body */}
+        <div
+          id="chatBox"
+          className="flex-grow p-4   bg-gray-900 overflow-y-auto"
         >
-          <i className="fas fa-paper-plane"></i>
-        </button>
+          {chat.map((msg, idx) => {
+            const createdAtDate = new Date(msg.createdAt);
+
+            const time = !isNaN(createdAtDate.getTime())
+              ? createdAtDate.toLocaleTimeString([], {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  hour12: true,
+                })
+              : "Invalid Date";
+
+            return (
+              <div
+                key={idx}
+                id="outer-msg-box"
+                className={`${
+                  msg.recipient === contactUserId
+                    ? "flex justify-end"
+                    : "flex justify-start"
+                }`}
+              >
+                {/* {msg.image ? <img src={msg.image} alt="Shared" className="w-auto h-[300px]" /> : null} */}
+                {msg.sender === contactUserId ? (
+                  !msg.image ? (
+                    <div className="m-5 p-4 relative text-xl text-white h-auto min-w-20  max-w-[200px] bg-[#474545] rounded-lg">
+                      {msg.content}
+                      <div className="w-full text-xs flex items-end justify-end absolute text-gray-200 right-2 bottom-0">
+                        {time}
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="m-5 p-4 relative text-xl text-white h-auto max-w-[200px] bg-[#474545] rounded-lg">
+                      {/* Render the image here */}
+                      <img
+                        src={msg.image}
+                        alt="Shared"
+                        className="w-auto h-auto object-cover"
+                      />
+                      {/* Optionally render the message content if needed */}
+                      {msg.content && <div className="mt-4">{msg.content}</div>}
+                      <div className="w-full  text-xs flex items-end justify-end absolute text-gray-200 right-2 bottom-0">
+                        {time}
+                      </div>
+                    </div>
+                  )
+                ) : (
+                  !msg.image ? (
+                  <div className="m-5 p-4 relative text-xl text-white h-auto min-w-20  max-w-[200px] bg-[#2d7d4a] rounded-lg">
+                    {msg.content}
+                    <div className="w-full text-xs flex items-end justify-end absolute text-gray-200 right-2 bottom-0">
+                      {time}
+                    </div>
+                  </div>
+                  ):
+                  (
+                    <div className="m-5 p-1 relative text-xl text-white h-auto max-w-[400px] bg-[#2d7d4a] rounded-lg">
+                      {/* Render the image here */}
+                      <img
+                        src={msg.image}
+                        alt="Shared"
+                        className="w-auto h-auto object-cover rounded-md"
+                      />
+                      {/* Optionally render the message content if needed */}
+                      {msg.content && <div className="mt-4
+                      ">{msg.content}</div>}
+                      <div className="w-full text-xs flex items-end justify-end absolute text-gray-200 right-2 bottom-0">
+                        {time}
+                      </div>
+                    </div>
+                  )
+                )}  
+              </div>
+            );
+          })}
+          <div ref={chatEndRef} />
+        </div>
+
+        {/* Bottom Input Section */}
+
+        <div className="flex items-center p-4 bg-gray-800">
+          <div className="flex items-center space-x-4">
+            <button className="p-2 text-gray-400 hover:text-white">
+              <i className="far fa-smile"></i>
+            </button>
+            <button className="p-2 text-gray-400 hover:text-white">
+              <div className="flex items-center justify-center">
+                <label className="cursor-pointer">
+                  <i className="fas fa-camera text-3xl text-gray-500 hover:text-blue-500"></i>
+                  <input
+                    type="file"
+                    className="hidden"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    
+                  />
+                </label>
+              </div>
+            </button>
+          </div>
+
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Type a message"
+            className="flex-grow bg-gray-700 text-white rounded-lg px-4 py-2 mx-4 focus:outline-none"
+          />
+          <button
+            onClick={sendMessage}
+            className="bg-green-500 p-3 rounded-full hover:bg-green-600"
+          >
+            <i className="fas fa-paper-plane"></i>
+          </button>
+        </div>
       </div>
-      
-    </div>
     </>
   );
 };
